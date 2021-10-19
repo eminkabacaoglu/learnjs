4691d187b694d8b0ef0bd8578669d5440bcc8db3
eminkabacaoglu <eminkabacaoglu@gmail.com>
2021-10-19 20:05:46 +0300

    functions and arrays

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ console.log(dolarDun) // 9.20 yazar çünkü let dolarDun = 9.10 farklı bir sco
 
 
 const euroDun = 11.2
-//euroDun = 11 // atama
+//euroDun = 11 // atama yapılamaz çünkü const
 
 console.log(euroDun) // consoleda hata verir çünkü const olarak tanımladığımız için başka değer atayamayız
 
@@ -53,3 +53,41 @@ for (let i = 0; i < konutKredileri.length; i++) {
     
 }
 console.log("</ul>")
+
+
+let student = {id:1,name:"Emin"} //obje
+console.log(student)
+console.log(student.id+" "+student.name)
+
+
+save()
+saveOgrenci(student.name)
+
+function save(){
+    console.log("Saved")
+}
+
+function saveOgrenci(ogrenci){
+    console.log(ogrenci+" Saved")
+}
+
+function saveOgrenciPuan(ogrenci,puan=10){
+    console.log(ogrenci+" Puanı: "+puan)
+}
+
+saveOgrenciPuan(student.name)
+saveOgrenciPuan(student.name,80)
+
+
+function fonkParamDeneme(puan=10, ogrenci){
+    console.log(ogrenci+" Puanı: "+puan)
+}
+fonkParamDeneme(undefined,student.name) // default parametreyi kullanmak için undefined demek lazım ya da default parametresi olanı sona taşımak lazım, birden fazla default varsa yine undefined kullanılabilir
+
+
+let student2 = [student,{id:2, name:"Can"}, "Ankara", {city:"İstanbul"}]
+
+console.log(student2)
+console.log(student2[1])
+console.log(student2[1].id+" "+student2[1].name)
+console.log(student2[3].city)
